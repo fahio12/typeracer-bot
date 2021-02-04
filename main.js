d5839dfabd525279c46ddb9ac36bb408402cf66d
Fabio Hernandez <44535804+fahio12@users.noreply.github.com>
2021-02-04 12:16:07 -0500

    gets players and prompts text

--- main.js
+++ main.js
@@ -2,30 +2,48 @@ require('dotenv').config()
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-startMessage = 'Game started. Press 👍 to play'
+startMessage = 'Game starting. Press 👍 to play. Press ✅ to start'
+players = []
 
 client.on('message', msg => {
+  //starts game
   if (msg.content === '#play') {
-    console.log(msg)
-    start(msg)
+    msg.channel.send(startMessage)
   }
+  //gets players
   if (msg.content === startMessage) {
-    console.log(msg)
-    start(msg)
+    msg.react('👍')
+    msg.react('✅')
   }
 });
 
+client.on('messageReactionAdd', (reaction, user) => {
+  // Get Players and adds them to players[] list
+    if(reaction.emoji.name === "👍") {
+      if(!user.bot && !players.includes(user.username)){
+        players.push(user.username)
+      }
+  // Starts game
+    }else if(reaction.emoji.name === "✅"){
+      if(!user.bot){
+        // add start function later
+        start(reaction)
+        reaction.message.delete()
+      }
+    }
+});
+async function start(reaction){
+  channel = reaction.message.channel
+  channel.send("✅ Game started ✅\nPlayers are:")
+  players.forEach(player => channel.send(player))
+  channel.send("Try typing the following text as fast as you can.")
+  // Change later so its dinamic
+  promptedText = await channel.send("`The pleasure of rooting for Goliath is that you can expect to win. The pleasure of rooting for David is that, while you don't know what to expect, you stand at least a chance of being inspired.`")
+  startTime = promptedText.createdTimestamp
+}
+
 client.login(process.env.TOKEN);
 
-function play(msg){
-  msg.react('👍')
-  msg.channel.send(startMessage)
-
-}
-function start(msg){
-  msg.react('👍')
-  
-}
 //get playerss
 //prompt text
 //players type
